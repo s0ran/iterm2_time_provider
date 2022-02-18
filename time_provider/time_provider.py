28e3e7ef917888be5b605efc89b2681b02df506b
s0ran <s0ran.n.11921@gmail.com>
2022-02-18 16:13:05 +0900

    second to minute

--- time_provider/time_provider.py
+++ time_provider/time_provider.py
@@ -10,31 +10,39 @@ import concurrent.futures
 
 def get_time():
     now=datetime.now()
-    string=now.strftime('%m月%d日 %H:%M:%S')
+    string=now.strftime('%m月%d日 %H:%M')
     return string
+
+def set_delay():
+    now=datetime.now()
+    eps=10**-2
+    return 60-now.second-now.microsecond*(10**-6)+eps
 
 
 async def main(connection):
     app=await iterm2.async_get_app(connection)
-    
-    print("call")
     loop = asyncio.get_running_loop()
     sessions=[]
+    async def set_time():
+        #print(signal.getitimer(signal.ITIMER_REAL))     
+        for session in sessions:
+            try:
+                str_time=get_time()
+                await session.async_set_variable("user.now_date_time",str_time)
+            except:
+                sessions.remove(session)
+        return str_time
+
     async with iterm2.NewSessionMonitor(connection) as mon:
         while True:
             session_id=await mon.async_get()
             session=app.get_session_by_id(session_id)
-            #print(session)
             sessions.append(session)
-            signal.setitimer(signal.ITIMER_REAL, 1, 1)
-            async def set_time():
-                for session in sessions:
-                    try:
-                        str_time=get_time()
-                        await session.async_set_variable("user.now_date_time",str_time)
-                    except:
-                        sessions.remove(session)
+            await set_time()
+            if signal.getitimer(signal.ITIMER_REAL)==(0,0):# ずれるなら条件変えて同期とる。
+                signal.setitimer(signal.ITIMER_REAL, set_delay(), 60)
             loop.add_signal_handler(signal.SIGALRM,lambda : asyncio.create_task(set_time()))
+
 
     
 
